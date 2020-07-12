026f19cc3ac10a0d65d54ad251b5296adc67d41b
Shi Sha <emailtotheowner@gmail.com>
2020-07-12 09:25:38 -0400

    Page navbar url fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -9,15 +10,20 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav justify-content-end ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/"> Home <span className="sr-only">(current)</span></a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              {/* <a className="nav-link" href="/"> Home <span className="sr-only">(current)</span></a> */}
+              <Link className="nav-link" to="/">
+              <i className="fas fa-home"></i> Home  </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/portfolio">Portfolio</a>
+            <Link className="nav-link" to="/portfolio">
+                <i className="fas fa-layer-group"> </i> Portfolio  </Link>
+              
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact">Contact</a>
+            <Link className="nav-link" to="/contact">
+               <i className="fas fa-id-card"> </i> Contact  </Link>
             </li>
           </ul>
         </div>
