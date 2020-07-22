39a18a4a4cb2b242d1713f0e641e5b893307bb7a
Shi Sha <emailtotheowner@gmail.com>
2020-07-22 11:50:43 -0400

    Replaced new resume

--- src/components/ContactResumeSection.jsx
+++ src/components/ContactResumeSection.jsx
@@ -6,7 +6,7 @@ export const ContactResumeSection = () => {
       <p className="aboutPara text-center"> In my spare time, I like doing natural landscape and portrait oil paintings. <br /> I have some knowledge on chalkboard writing and digital drawing. </p>
       <p className="aboutPara text-center">If you are interested to view some of my works, click the button below. </p>
       <hr className="contactHr" />
-      <a href="../../images/Shiva-Resume-Web-Dev.pdf" target="_blank" id="resume-button"> <button type="button" className="btn">Download Resumé</button> </a>
+      <a href="../../images/Shiva-Resume-Web-Dev-2020.pdf" target="_blank" id="resume-button"> <button type="button" className="btn">Download Resumé</button> </a>
       <a href="https://personal-react-site.netlify.app/" target="_blank" rel="noopener noreferrer" id="personal-webpage-button"> <button type="button" className="btn">Go To My Personal Page</button> </a>
     </>
   )
